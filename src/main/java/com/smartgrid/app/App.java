5b465e6cdad90e6b4bd472fb907f5473635f8c13
Robin Edwards <robin.ge@gmail.com>
2012-03-06 15:43:12 +0000

    added simulator class and setup in main
    - simulator will now run

--- src/main/java/com/smartgrid/app/App.java
+++ src/main/java/com/smartgrid/app/App.java
@@ -1,13 +1,34 @@
 package com.smartgrid.app;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import com.smartgrid.app.Simulator;
+import com.smartgrid.policies.AggregatorPolicy;
+import com.smartgrid.policies.CustomAggregatorPolicy;
+import com.smartgrid.policies.CustomHouseholdPolicy;
+import com.smartgrid.policies.HouseholdPolicy;
+
+import java.util.HashMap;
+
+public class App {
+    public static void main( String[] args ) {
+    	System.out.println("Setting up simulator");
+    	
+    	Integer population = 50;
+    	
+    	// TODO the wrapping of household policy within a policy object should happen in the simulator.
+    	HashMap<Integer,Household> households = new HashMap<Integer,Household>();
+    	for (Integer i = 1; i <= population; i++) {
+    		Household h = new Household(i, new CustomHouseholdPolicy());
+    		households.put(i, h);
+    	}
+    	
+    	Integer granularity = 60 * 60; // in seconds
+    	Long iterations = 10000L;
+    	AggregatorPolicy aggregatorPolicy =  new CustomAggregatorPolicy();
+    
+    	Simulator simulator = new Simulator(households, iterations, granularity, aggregatorPolicy);
+    	
+        System.out.println("Running simulator");
+        simulator.run();
+        System.out.println("Simulation finished");
     }
 }
--- src/main/java/com/smartgrid/app/Simulator.java
+++ src/main/java/com/smartgrid/app/Simulator.java
@@ -1,15 +1,58 @@
 package com.smartgrid.app;
 
 import java.util.HashMap;
+import java.util.GregorianCalendar;
+import java.util.Calendar;
+import java.util.Date;
+import com.smartgrid.app.Aggregator;
+import com.smartgrid.messenger.Message;
+import com.smartgrid.messenger.Messenger;
+import com.smartgrid.messenger.MessengerBasic;
+import com.smartgrid.policies.AggregatorPolicy;
+import com.smartgrid.policies.HouseholdPolicy;
 
 public class Simulator {
-	private Long ticks;
-	private Integer granularity;
-	private HashMap<Integer,Household> households;
+	private Long tick = 0L; // current tick
+	private Long iterations; // total number of iterations
+	private Long startTime; // start time epoch (in milliseconds)
+	private Long currentTime; // end time epoch (in milliseconds)
+	private Messenger<Household> messenger;
+	private Integer granularity; // seconds per tick
+	private AggregatorPolicy aggregatorPolicy;
+	private Aggregator aggregator;
 	
-	Simulator(HashMap<Integer,Household> h, Long t, Integer g) {
-		ticks = t;
-		granularity = g;
-		households = h;
+	public Simulator(HashMap<Integer,Household> households,
+			Long iterations,
+			Integer granularity,
+			AggregatorPolicy aggregatorPolicy
+			) {
+		this.iterations  = iterations;
+		this.granularity = granularity;
+		this.aggregatorPolicy = aggregatorPolicy;
+		
+		messenger = new MessengerBasic<Household>(households);
+		aggregator = new Aggregator(messenger);
+
+		this.aggregatorPolicy.setup(aggregator);
+	}
+	
+	private void tick(Date date) {
+		// execute each household tick TODO: use messenger
+		messenger.<Void,Date>messageMany(messenger.memberIds(), new Message<Date>("tick", date));
+		// execute aggregator policy tick.
+		aggregator.updateHouseholdDemands();
+		aggregatorPolicy.tick(date, aggregator);
+	}
+	
+	public void run() {
+		Calendar cal = GregorianCalendar.getInstance();
+		startTime = cal.getTimeInMillis();
+		currentTime = startTime;
+
+		while (tick <= iterations) {
+			tick++;
+			currentTime += 1000L * granularity;
+			tick(new Date(currentTime));
+		}
 	}
 }
